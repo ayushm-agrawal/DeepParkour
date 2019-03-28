876e6e724dca9da2ce570f81d4036c7b0b45d63a
Atharva Tendle <atharva.tendle@huskers.unl.edu>
2019-03-28 16:12:01 -0500

    Further Updates
    Add argument parsing and logging for efficient tracking for the training loop.

--- src/main/test_run.py
+++ src/main/test_run.py
@@ -1,33 +1,65 @@
+import argparse
 import gym
-import numpy as np
-import os.path, sys, io
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
 import pybullet_envs
-import time
 from baselines import deepq
-import numpy as np
+
+
+# setup parser
+parser = argparse.ArgumentParser(description='Train Humanoid Agent.')
+parser.add_argument(
+    '--model_dir',
+    type=str,
+    default='/home/cse496dl/atendle/Final_Project/DeepParkour/agents',
+    help='directory where agents are saved')
+parser.add_argument('--batch_size', type=int, default=64, help='mini batch size for training')
+parser.add_argument('--network', type=int, default='mlp', help='model/agent to use')
+parser.add_argument('--total_timesteps', type=int, default=100000, help='number of timesteps to run')
+parser.add_argument('--learning_rate', type=float, default=5e-4, help='Learning rate for optimizer')
+parser.add_argument('--gamma', type=float, default=1.0, help='discount factor')
+parser.add_argument('--print_frequency', type=float, default=100, help='Frequency of prints')
+parser.add_argument('--exp_frac', type=float, default = 0.1, help='fraction of entire training period over which the exploration rate is annealed')
+parser.add_argument('--exp_final', type=float, default = 0.02, help='final value of random action probability')
+parser.add_argument('--buffer', type=int, default=50000, help='size of the replay buffer')
+parser.add_argument('--render', type=int, default=0, help='flag to render(1 == render) (0 == do not render)')
+# setup parser arguments
+args = parser.parse_args()
 
 def main():
-    # create and render environment
+    # create environment
     env = gym.make("HumanoidBulletEnv-v0")
-    # env.render(mode="human")
-    # get shape of actions
-    NUM_ACTIONS = env.action_space.shape[0]
-    # get shape of observations
-    OBS_SHAPE = env.observation_space.shape[0]
-
+    # render environment
+    if args.render == 1:
+        env.render(mode="human")
+        
+    # log necessary hyperparameters
+    print('Number of observations collected by the Environment: {}'.format(env.observation_space.shape[0]))
+    print('Number of actions an agent can take: {}'.format(env.action_space.shape[0]))
+    print('Batch Size: {}'.format(args.batch_size))
+    print('Discount Factor: {}'.format(args.gamma))
+    print('Type of Network: {}'.format(args.network))
+    print('Learning Rate: {}'.format(args.learning_rate)
+    print('Total Timesteps: {}'.format(args.total_timesteps))
+    print('Buffer Size: {}'.format(args.buffer))
+    print('Exploration Fraction: {}'.format(args.exp_frac))
+    print('Exploration Final Episode: {}'.format(args.exp_final))
+      
+   
+    
+    # train the agent using learning algorithm
     act = deepq.learn(
         env,
-        network='mlp',
-        lr=1e-3,
-        total_timesteps=100000,
-        buffer_size=50000,
-        exploration_fraction=0.1,
-        exploration_final_eps=0.02,
-        print_freq=10,
-        callback=callback
+        batch_size=args.batch_size,
+        gamma=args.gamma,
+        network=args.network,
+        lr=args.learning_rate,
+        total_timesteps=args.total_timesteps,
+        buffer_size=args.buffer,
+        exploration_fraction=args.exp_frac,
+        exploration_final_eps=args.exp_final,
+        print_freq=args.print_frequency
     )
-    print("Saving model to humanoid_model.pkl")
+          
+    print("Saving model")
     act.save("humanoid_model.pkl")
 
 if __name__ == '__main__':
